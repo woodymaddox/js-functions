7343f0e6a1aceca6f4f446e1775fc8b995f59514
Woody Maddox <woody.e.maddox@gmail.com>
2019-03-02 13:47:13 -0600

    updated

--- main.js
+++ main.js
@@ -21,16 +21,38 @@ namePrinter('Zoe', 'Ames');
 namePrinter('John', 'Doe');
 
 //namePrinter(1,2);
-
+*/
 const nuggetizer =(animal) => {
 return `processed ${animal}`;
 };
 
 console.log(nuggetizer('pig'));
-*/
+
 
 
 const dogbreedPrinter=(dogBreed)=>{
 return `My favorite dog breed is ${dogBreed}`;
 };
 console.log(dogbreedPrinter('Lab'));
+
+const dogBreedDiv = document.getElementById('dog-breeds');
+console.log('dogBreedDiv', dogBreedDiv);
+
+dogBreedDiv.innerHTML ='<h3>hi im an h3</h3>';
+
+
+const nuggetDiv = document.getElementById('spicyNuggets');
+
+spicyNuggets.innerHTML = nuggetizer('bear');
+spicyNuggets.innerHTML += nuggetizer('chicken');
+
+const printToDom = (divId, textToPrint) => {
+const selectedDiv = document.getElementById (divId);
+selectedDiv.innerHTML += textToPrint;
+
+
+printToDom('dog-breeds', dogBreed ('lab'))
+
+};
+
+printToDom('spicyNuggets', 'mmmmmm');
